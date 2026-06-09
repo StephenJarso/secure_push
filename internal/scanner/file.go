f6a6d85676bbae80e43c21046aeb3bb9c5efdd8f
sjarso <stephenjacob815@gmail.com>
2026-06-09 09:36:59 +0300

    feat: add binary file detection and skipping

--- internal/scanner/file.go
+++ internal/scanner/file.go
@@ -1,1 +1,47 @@
 package scanner
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func IsBinaryFile(path string) (bool, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return false, err
+	}
+
+	return IsBinary(buffer), nil
+}
+
+func IsBinary(data []byte) bool {
+	for _, b := range data[:min(len(data), 512)] {
+		if b == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func GetFileExtension(path string) string {
+	return filepath.Ext(path)
+}
+
+func IsTextFile(path string) (bool, error) {
+	isBinary, err := IsBinaryFile(path)
+	return !isBinary, err
+}
