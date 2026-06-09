15088f48caef2332340c9e3f478f0c5508f7352f
sjarso <stephenjacob815@gmail.com>
2026-06-09 09:37:22 +0300

    feat: add file walking utilities for scanner

--- internal/scanner/walk.go
+++ internal/scanner/walk.go
@@ -1,1 +1,71 @@
 package scanner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func WalkDir(path string, visit func(path string, info os.FileInfo) error) error {
+	return filepath.Walk(path, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			if strings.HasPrefix(filepath.Base(filePath), ".") && filepath.Base(filePath) != "." {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+
+		return visit(filePath, info)
+	})
+}
+
+func GetFileSize(path string) (int64, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return 0, err
+	}
+	return info.Size(), nil
+}
+
+func FileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func IsDir(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return info.IsDir()
+}
+
+func GetRelativePath(base, path string) (string, error) {
+	rel, err := filepath.Rel(base, path)
+	if err != nil {
+		return "", err
+	}
+	return filepath.ToSlash(rel), nil
+}
+
+func ValidatePath(path string) error {
+	if path == "" {
+		return fmt.Errorf("path cannot be empty")
+	}
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("path does not exist: %w", err)
+	}
+
+	if !info.IsDir() {
+		return fmt.Errorf("path is not a directory: %s", path)
+	}
+
+	return nil
+}
